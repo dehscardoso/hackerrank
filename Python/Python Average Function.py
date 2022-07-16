ea06801e8bc61c679ad39fdda3f948a8deec305a
dehscardoso <dsc7026@gmail.com>
2022-07-16 10:01:58 -0300

    add files

---
+++ Python/Python Average Function.py
@@ -0,0 +1,15 @@
+'''
+Implement a function that:
+1. Is named avg
+2. Takes a variable number of integer arguments; it is guaranteed that at least one argument            will be passed
+3. Returns the average value of the passed arguments as a float
+
+The implementation will be tested by a provided code stub on several input files. Each input file contains one line with space-separated arguments for the function. The function will be called with those arguments, and the returned result will be printed to the output with exactly 2 decimal places.
+
+Example
+3 arguments are read and passed to the function: 1, 2, and 3. 
+The average is calculated to be (1+2+3)/3 =2.00. 
+This is then returned as a float to be printed.'''
+
+def avg(*num):
+    return sum(nums)/len(nums)
